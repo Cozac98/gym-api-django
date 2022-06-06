9260f1efbfaffc4e39e9e8b394ba9bc34579e671
Cozac98 <ale.cozacov@gmail.com>
2022-06-06 18:27:04 -0300

    viewsets

--- routines/urls.py
+++ routines/urls.py
@@ -1,12 +1,13 @@
-from .views import ListCreateRoutine, RetrieveUpdateDeleteRoutine
 from django.urls import path
+
+from .views import RoutineViewSet
 
 
 urlpatterns = [
-    path("", ListCreateRoutine.as_view(), name="list-create-routines"),
+    path("", RoutineViewSet.as_view({"get": "list", "post": "create"}), name="list-create-routines"),
     path(
         "<int:pk>/",
-        RetrieveUpdateDeleteRoutine.as_view(),
+        RoutineViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}),
         name="retrieve-update-delete-routine",
     ),
 ]
--- routines/views.py
+++ routines/views.py
@@ -1,19 +1,9 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import viewsets
 
 from routines.models import Routine
 from routines.serializers import RoutineSerializer
 
 # Create your views here.
-class ListCreateRoutine(ListCreateAPIView):
+class RoutineViewSet(viewsets.ModelViewSet):
     queryset = Routine.objects.all()
     serializer_class = RoutineSerializer
-
-
-class RetrieveUpdateDeleteRoutine(RetrieveUpdateDestroyAPIView):
-    queryset = Routine.objects.all()
-    serializer_class = RoutineSerializer
-
-    def partial_update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
