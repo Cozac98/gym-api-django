3f5efe10bb8f8c8528263974e9e7981b12f34ba6
Cozac98 <ale.cozacov@gmail.com>
2022-06-29 18:07:29 -0300

    .

--- api/settings.py
+++ api/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = getenv("SECRET_JWT_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gym-austral-tp.herokuapp.com']
 
 
 # Application definition
