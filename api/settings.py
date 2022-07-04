42cd9c9e01d94a3aad8f84ea4ce60282618f2331
Cozac98 <ale.cozacov@gmail.com>
2022-07-04 08:36:30 -0300

    cors

--- api/settings.py
+++ api/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_yasg",
