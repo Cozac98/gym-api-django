dd2131b319b19b4185027bc8790f6fd3641eab3a
Cozac98 <ale.cozacov@gmail.com>
2022-07-17 22:45:51 -0300

    .

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.decorators import api_view, permission_classes
-
+from django.contrib.auth.models import User
 from .serializers import StaffSerializer, UserSerializer
 
 class RegisterUserAPIView(generics.CreateAPIView):
@@ -16,3 +16,13 @@ class RegisterStaffAPIView(generics.CreateAPIView):
 @permission_classes([IsAuthenticated])
 def me(request):
     return response.Response(UserSerializer(request.user).data, 200)
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def users(request):
+    return response.Response(UserSerializer(request.user).data, 200)
+
+class GetUsersAPIView(generics.ListAPIView):
+  queryset = User.objects.all()
+  permission_classes = [IsAdminUser]
+  serializer_class = UserSerializer
