74fb1220a9d1aa591599ab61a93e07b4f747f9c4
Cozac98 <ale.cozacov@gmail.com>
2022-07-18 17:55:35 -0300

    .

--- routines/serializers.py
+++ routines/serializers.py
@@ -5,8 +5,13 @@ from routine_day.serializers import RoutineDaySerializerWithRelationship
 from routines.models import Routine
 
 
-class RoutineSerializer(serializers.ModelSerializer):
+class RoutineWithRelationSerializer(serializers.ModelSerializer):
     routine_days = RoutineDaySerializerWithRelationship(read_only=True, many=True)
+    class Meta:
+        model = Routine
+        fields = "__all__"
+
+class RoutineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Routine
         fields = "__all__"
