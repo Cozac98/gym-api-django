15ed29cb2883c4976d24469d8ec2cb25ce4089d0
Cozac98 <alejandrocozacov@sirius.com.ar>
2022-12-04 17:20:21 -0300

    changes in excercise model

--- exercise/models.py
+++ exercise/models.py
@@ -5,5 +5,5 @@ from muscle_groups.models import MuscleGroup
 # Create your models here.
 class Exercise(models.Model):
     name = models.CharField(max_length=30, null=False)
-    description = models.CharField(max_length=120, null=False)
+    description = models.CharField(max_length=255, null=False)
     muscle_group = models.ForeignKey(MuscleGroup, on_delete=models.CASCADE, null=False)
